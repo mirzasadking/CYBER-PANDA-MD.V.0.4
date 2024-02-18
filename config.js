6ffb3356281714a451d949b45cdfc4bc186f96e8
mirzasadking <152530240+mirzasadking@users.noreply.github.com>
2024-02-19 01:21:04 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ global.antitags = true;
 
 
 //auto functioner
-global.autoTyping = false;                //auto tying by default off.
+global.autoTyping = true;                //auto tying by default off.
 global.autoRecord = false;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
 global.statusseen = false;                 //This is the new variable for controlling status seen.
@@ -22,10 +22,10 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 //
 global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
-global.BotName = "SACHITH";
+global.OwnerNumber = ["+18125121353", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+18125121353"];
+global.OwnerName = "mirza";
+global.BotName = "mirza";
 global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
 global.author = "SACHITH";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
